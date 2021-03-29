78d005a3641c2ab74de64e4b0e5c32c4eb7c198f
Paul <pfvatterott@gmail.com>
2021-03-29 16:57:54 -0600

    removed login page so index goes directly to strava

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-router.get('/login', (req, res) => {
-    res.render('login');
-});
-
 router.get('/logout', (req, res) => {
     req.logout();
     res.render('index');
@@ -14,7 +10,6 @@ router.get('/strava', passport.authenticate('strava'),
     function (req, res) {
         
     });
-
 
 //   callback route for strava to redirect to
 router.get('/strava/redirect', passport.authenticate('strava', { failureRedirect: '/login' }), (req, res) => {
